9631943413ef472621c6f61366bbd4b245608d80
jinpeng.li <eddielee111@yahoo.com>
2015-05-26 06:41:09 +0100

    init front end project, hide and show side panel

---
+++ app/views/application.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  didInsertElement : function(){
+          // Menu settings
+          Ember.$('#menuToggle, .menu-close').on('click', function(){
+            Ember.$('#menuToggle').toggleClass('active');
+            Ember.$('body').toggleClass('body-push-toleft');
+            Ember.$('#theMenu').toggleClass('menu-open');
+          });
+
+  }
+});
